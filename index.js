b0574cc360f99eed4527d735e88dc8bb3db3242b
Bishoeri000 <Bishoeri000@warren.k12.in.us>
2025-01-24 12:23:37 -0500

    Second Commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Happy developing ✨')
+let x = 10
+
+const executor = (resolve) => {
+    resolve(x)
+}
+
+const promise = new Promise(executor);
+
+promise
+    .then(number => number * 2)
+    .then(number => number + 10)
+    .then(number => number - 5)
+    .then(number => console.log(number));
